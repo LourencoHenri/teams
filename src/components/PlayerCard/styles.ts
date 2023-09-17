758138caa8426eb8d567d7c13691a0f61a580e5e
Lourenço <henri.lourenco@outlook.com>
2023-09-17 20:24:03 -0300

    [Add] Update

--- src/components/PlayerCard/styles.ts
+++ src/components/PlayerCard/styles.ts
@@ -26,20 +26,20 @@ export const Icon = styled(MaterialIcons).attrs(({ theme }) => ({
 	margin-right: 4px;
 `;
 
-/* export const Container = styled(TouchableOpacity)<Props>`
-	justify-content: center;
-	align-items: center;
-	border-radius: 6px;
-	margin-left: 12px;
-	max-height: 56px;
-	min-width: 56px;
-	flex: 1; */
+// /* export const Container = styled(TouchableOpacity)<Props>`
+// 	justify-content: center;
+// 	align-items: center;
+// 	border-radius: 6px;
+// 	margin-left: 12px;
+// 	max-height: 56px;
+// 	min-width: 56px;
+// 	flex: 1; */
 
-// 	background-color: ${({ theme, type }) =>
-// 		type === "primary" ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
-// `;
+// // 	background-color: ${({ theme, type }) =>
+// // 		type === "primary" ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
+// // `;
 
-// export const Icon = styled(MaterialIcons).attrs<Props>(({ theme, type }) => ({
-// 	size: 24,
-// 	color: type === "primary" ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK,
-// }))``;
+// // export const Icon = styled(MaterialIcons).attrs<Props>(({ theme, type }) => ({
+// // 	size: 24,
+// // 	color: type === "primary" ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK,
+// // }))``;
